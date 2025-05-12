9222a4705f1f78931904ea079c8ce5b2f23566dc
Khaled Omar <khaledomar3112@gmail.com>
2025-05-12 19:14:33 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,5 @@
 CREATE INDEX idx_booking_start_date ON booking(start_date);
 CREATE INDEX idx_property_location ON property(location);
 CREATE INDEX idx_property_price ON property(pricepernight);
+
+EXPLAIN ANALYZE SELECT * FROM bookings WHERE user_id = 42 ORDER BY booking_date DESC;
