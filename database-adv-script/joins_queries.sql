a20c195335dea467192001b23ec6de08a4f06837
Khaled Omar <khaledomar3112@gmail.com>
2025-05-12 15:46:31 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,11 +3,14 @@ FROM booking
 INNER JOIN user ON user.user_id = booking.user_id;
 
 SELECT user.first_name, user.last_name, start_date, end_date, total_price, status
-FROM booking LEFT JOIN user on user.user_id = booking.user_id;
+FROM booking LEFT JOIN user on user.user_id = booking.user_id
+ORDER BY user.id;
+
 
 SELECT *
 FROM booking
 LEFT JOIN user ON user.user_id = booking.user_id
+
 
 UNION
 
